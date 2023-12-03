1fcbf2296455bbb0f89713126cdf81f6c1df8e35
Aleksey-Dr <aprel181818@gmail.com>
2023-12-03 13:23:16 +0200

    Add Color switcher
    Add Color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+// COLOR SWITCHER
+console.log('Color switcher');
 
+// ========== find elements
+const bodyElement = document.querySelector('body');
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+// ========== variable for interval id
+let switcherColor = null;
+
+// ========== default state for the "Stop" button
+btnStop.disabled = true;
+
+// ========== colors generator
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+// ========== handler of events
+const handlerStart = () => {
+    switcherColor = setInterval(() => {
+        bodyElement.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+};
+
+const handlerStop = () => {
+    clearInterval(switcherColor);
+
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+};
+
+// ========== listeners
+btnStart.addEventListener('click', handlerStart);
+btnStop.addEventListener('click', handlerStop);
